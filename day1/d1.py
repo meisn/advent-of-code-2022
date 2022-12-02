8241196cdc863c5a0f12ad9b2e6ef9c5c6c61d8a
meisn <meissner.ronny@gmail.com>
2022-12-02 11:37:25 +0100

    Day1: Init

---
+++ day1/d1.py
@@ -0,0 +1,15 @@
+with open("../files/AoC22-d1.txt") as f:
+    raw = f.read()
+
+inp = []
+for i, it in enumerate(raw.split("\n\n")):
+
+    dat = [int(i) for i in it.split("\n")]
+    s = sum(dat)
+    inp.append((i, it, s))
+
+inp.sort(key=lambda x: x[2], reverse=True)
+answer1 = inp[0][2]
+answer2 = sum(i[2] for i in inp[:3])
+print(f"Answer 1: {answer1}")
+print(f"Answer 2: {answer2}")
