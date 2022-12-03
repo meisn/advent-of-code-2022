a33b7afe092b73a9c55fda3943ea7cb0a778c681
meisn <meissner.ronny@gmail.com>
2022-12-03 20:26:59 +0100

    file location changed

--- rust/d1/src/main.rs
+++ rust/d1/src/main.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::fs;
 
 fn main() {
-    let rawdata = fs::read_to_string(r".\file\d1.txt").expect("Error Reading File");
+    let rawdata = fs::read_to_string(r"..\..\files\AoC22-d1.txt").expect("Error Reading File");
 
     let re = Regex::new(r"\r\n\r\n").unwrap();
     let data: Vec<&str> = re.split(&rawdata).collect();
