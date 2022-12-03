a188f365f743cd4119709a63c70863521815941c
meisn <meissner.ronny@gmail.com>
2022-12-03 20:25:14 +0100

    rust init

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Advent of Code 2022
+
+My own humble attempts to solve the puzzles.
+
+As usual, will try first in Python and this year also in Rust, whih will take me some time as I learn it still.
---
+++ rust/d1/src/main.rs
@@ -0,0 +1,28 @@
+use regex::Regex;
+use std::fs;
+
+fn main() {
+    let rawdata = fs::read_to_string(r".\file\d1.txt").expect("Error Reading File");
+
+    let re = Regex::new(r"\r\n\r\n").unwrap();
+    let data: Vec<&str> = re.split(&rawdata).collect();
+
+    let mut nums: Vec<i32> = Vec::new();
+    for item in &data {
+        let sum: i32 = item
+            .lines()
+            .map(|x| x.trim().parse::<i32>().unwrap())
+            .collect::<Vec<i32>>()
+            .iter()
+            .sum();
+        nums.push(sum);
+    }
+
+    nums.sort();
+    nums.reverse();
+
+    println!("{}", data.len());
+    println!("Answer1: {}", nums[0]);
+    let ans2 = nums[0] + nums[1] + nums[2];
+    println!("Answer2: {}", ans2);
+}
