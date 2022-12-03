e3ccb998d0edd6653abec0822e5559af0c6aba92
meisn <meissner.ronny@gmail.com>
2022-12-03 10:14:34 +0100

    formatting

--- day2/d2.py
+++ day2/d2.py
@@ -2,6 +2,7 @@ opponent = dict(zip("ABC", range(1, 4)))
 player = dict(zip("XYZ", range(1, 4)))
 results = dict(zip("XYZ", range(3)))
 
+
 def calc_result1(opponent_in: str, player_in: str) -> int:
     o_val = opponent.get(opponent_in, 0)
     p_val = player.get(player_in, 0)
@@ -22,31 +23,32 @@ def calc_result1(opponent_in: str, player_in: str) -> int:
 
 
 def calc_result2(opponent_in: str, player_in: str) -> int:
-    
+
     val = ""
     match (opponent_in, player_in):
-        case ('A', 'X'):
-            val = 'C'
-        case ('A', 'Y'):
-            val = 'A'
-        case ('A', 'Z'):
-            val = 'B'
-        case ('B', 'X'):
-            val = 'A'
-        case ('B', 'Y'):
-            val = 'B'
-        case ('B', 'Z'):
-            val = 'C'
-        case ('C', 'X'):
-            val = 'B'
-        case ('C', 'Y'):
-            val = 'C'
-        case ('C', 'Z'):
-            val = 'A'
-    
+        case ("A", "X"):
+            val = "C"
+        case ("A", "Y"):
+            val = "A"
+        case ("A", "Z"):
+            val = "B"
+        case ("B", "X"):
+            val = "A"
+        case ("B", "Y"):
+            val = "B"
+        case ("B", "Z"):
+            val = "C"
+        case ("C", "X"):
+            val = "B"
+        case ("C", "Y"):
+            val = "C"
+        case ("C", "Z"):
+            val = "A"
+
     result = results.get(player_in, 0)
     p_val = opponent.get(val, 0)
     return result * 3 + p_val
+
 
 if __name__ == "__main__":
 
